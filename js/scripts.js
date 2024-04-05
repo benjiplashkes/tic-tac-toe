50f192935dfd62086d6b65f44d0304b3b0e666e0
Benji Plashkes <8240757+benjiplashkes@users.noreply.github.com>
2024-04-05 19:38:45 +0300

    Add Board Functions
    Add a function to log the getter functions to the console.
    Add a function to render the board to the console (ascii graphics).
    Add the addMove() function for adding move data to the board.

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,62 @@
-(function boardFactory (){
+(function boardFactory() {
   data = [
-    [
-      [0],[0],[0]
-    ],[
-      [0],[0],[0]
-    ],[
-      [0],[0],[0]
-    ]    
-  ]
-  return board = {
-    getRows: ()=>{
-      let rows = ""
+    [[0], [0], [0]],
+    [[0], [0], [0]],
+    [[0], [0], [0]],
+  ];
+  return (board = {
+    getRows: () => {
+      let rows = "";
       for (const row of data) {
-        rows += row.join("")
+        rows += row.join("");
       }
-      return rows
+      return rows;
     },
-    getColumns: ()=>{
-      let col1 = ""
-      let col2 = ""
-      let col3 = ""
-      
+    getColumns: () => {
+      let col1 = "";
+      let col2 = "";
+      let col3 = "";
+
       for (const row of data) {
-        col1 += row[0]
-        col2 += row[1]
-        col3 += row[2]
+        col1 += row[0];
+        col2 += row[1];
+        col3 += row[2];
       }
-      return col1 + col2 + col3
+      return col1 + col2 + col3;
     },
-    getDiagonals: ()=>{
-      let diag1 = ""
-      let diag2 = ""
+    getDiagonals: () => {
+      let diag1 = "";
+      let diag2 = "";
       for (let index = 0; index < data.length; index++) {
-        let reverseIndex = data.length -1
+        let reverseIndex = data.length - 1;
         const row = data[index];
-        diag1 += row[index]
-        diag2 += row[reverseIndex]
-        reverseIndex --        
+        diag1 += row[index];
+        diag2 += row[reverseIndex];
+        reverseIndex--;
       }
-      return diag1 + diag2
+      return diag1 + diag2;
     },
-    
-  }
-})()
+    addMove: (row, column, sign) => {
+      data[row][column] = sign;
+    },
+    log: () => {
+      console.log(`
+      Rows:       ${board.getRows()}
+      Columns:    ${board.getColumns()}
+      Diagonals:  ${board.getDiagonals()}
 
-console.log(
-  `Get Rows: ${board.getRows()}`,"\n",
-  `Get Cols: ${board.getColumns()}`,"\n",
-  `Get Diags: ${board.getDiagonals()}`,"\n",
+      `);
+    },
 
-  
-)
+    render: () => {
+      console.clear();
+      console.log("-------------");
+
+      for (let index = 0; index < data.length; index++) {
+        const row = data[index];
+        console.log("| " + row.join(" | ") + " |");
+        console.log("-------------");
+      }
+    },
+  });
+})();
