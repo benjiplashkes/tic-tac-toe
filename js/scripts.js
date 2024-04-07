0e133a8a32090e2cc7c861d3d8954b313aa4abc3
Benji Plashkes <8240757+benjiplashkes@users.noreply.github.com>
2024-04-07 18:18:01 +0300

    Add UI
    Add first version of html + css UI, handle cell clicking, choosing player names etc. from UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
-
-const board = (function() {
-
-  const data = [
+const board = (function () {
+  let data = [
     [[0], [0], [0]],
     [[0], [0], [0]],
     [[0], [0], [0]],
@@ -9,7 +7,7 @@ const board = (function() {
   const getRows = () => {
     let rows = "";
     for (const row of data) {
-      rows += row.join("");
+      rows += row.join("") + ",";
     }
     return rows;
   };
@@ -23,7 +21,7 @@ const board = (function() {
       col2 += row[1];
       col3 += row[2];
     }
-    return col1 + col2 + col3;
+    return `${col1}, ${col2},${col3}`
   };
   const getDiagonals = () => {
     let diag1 = "";
@@ -35,10 +33,9 @@ const board = (function() {
       diag2 += row[reverseIndex];
       reverseIndex--;
     }
-    return diag1 + diag2;
+    return `${diag1}, ${diag2}`;
   };
-  
-  
+
   /**
    * Description
    * @param {number} row
@@ -47,19 +44,24 @@ const board = (function() {
    * @returns {void}
    */
   const addMove = (row, column, sign) => {
-    if(data[row][column] === "X" || data[row][column] === "O") throw "Cell Not Empty"
-    if(data[row][column] == 0 ) {
-      data[row][column] = sign
+    if (data[row][column] === "X" || data[row][column] === "O")
+      throw "Cell Not Empty";
+    if (data[row][column] == 0) {
+      data[row][column] = sign;
     }
   };
   const reset = () => {
-    for(row of data){
-      for(cell of row){
-        row[cell] = 0
-      }
-    }
-
-  }
+    userInterface.reset()
+    return data = [
+      [
+        [0],[0],[0]
+      ],[
+        [0],[0],[0]
+      ],[
+        [0],[0],[0]
+      ]
+    ]
+  };
   const log = () => {
     console.log(`
       Rows:       ${board.getRows()}
@@ -80,14 +82,14 @@ const board = (function() {
     }
   };
 
-  return ( {getRows, getColumns, getDiagonals, addMove, reset, log, render} );
-})()
+  return { getRows, getColumns, getDiagonals, addMove, reset, log, render };
+})();
 
-function game() {
+const game = (function () {
   //Game State
   let gameState = "Playing";
   let isPlaying = gameState === "Playing" ? true : false;
-  
+
   // Player Data
   /**
    * Description
@@ -99,20 +101,24 @@ function game() {
   const Player = function (name, sign, score) {
     return { name, sign, score };
   };
-
-  
-  const player1 = new Player(
-    prompt("Please enter a name for PLAYER 1:", ""),
-    "X",
-    0
-  );
-  const player2 = new Player(
-    prompt("Please enter a name for PLAYER 2:", ""),
-    "O",
-    0
-  );
-  let currentPlayer = player1;
-
+  let player1;
+  let player2;
+  let currentPlayer;
+  const createPlayers = (name1, sign1, score1, name2, sign2, score2) => {
+    player1 = new Player(name1, sign1, score1);
+    player2 = new Player(name2, sign2, score2);
+    currentPlayer = player1;
+  };
+  const getCurrentPlayer = () => {
+    return currentPlayer;
+  };
+  const setCurrentPlayer = () => {
+    if (currentPlayer === player2) {
+      return currentPlayer = player1;
+    } else {
+      return currentPlayer = player2;
+    }
+  };
 
   // Game Logic Functions
   /**
@@ -133,20 +139,27 @@ function game() {
       throw "Error: wrong sign entered";
     }
 
-    board.addMove(row, cell, sign);
-  };
-  const getMove = () => {
-    // const row = Number(prompt(`${currentPlayer.name} select row:`));
-    // const column = Number(prompt(`${currentPlayer.name} select column:`));
+    let boardState = {
+      rows: board.getRows(),
+      columns: board.getColumns(),
+      diagonals: board.getDiagonals(),
+    };
 
-    if (row > 2 || row < 0) {
-      throw "getMove() - wrong row size ";
+    board.addMove(row, cell, sign);
+    boardState = {
+      rows: board.getRows(),
+      columns: board.getColumns(),
+      diagonals: board.getDiagonals(),
+    };
+    if (
+      checkEndGame(boardState.rows, boardState.columns, boardState.diagonals)
+    ) {
+      endGame();
+      return;
     }
-    if (column > 2 || column < 0) {
-      throw "getMove() - wrong column size";
-    }
-    return { row, column: column };
+    
   };
+
   /**
    * Description
    * @param {"win" || "tie" || "Playing"} state
@@ -155,29 +168,27 @@ function game() {
    */
   const endGame = () => {
     if (gameState === "win") {
-      console.clear();
-      board.render();
+      board.reset();
       console.log(`
         Player ${currentPlayer.name} "${currentPlayer.sign}" WINS !!!
         *************************
       `);
+      alert(`Player ${currentPlayer.name} ${currentPlayer.sign} WINS!!!`)
       currentPlayer.score++;
       isPlaying = false;
     }
     if (gameState === "tie") {
-      console.clear();
-      board.render();
+      board.reset();
       console.log(`
       GAME OVER !!!
       the game is tied
       `);
+      alert(`Game Over : Game is a tie!`)
       isPlaying = false;
     }
     if (!gameState === "Playing") {
       isPlaying = false;
     }
-    board.resetBoard()
-
   };
 
   /**
@@ -189,6 +200,8 @@ function game() {
    */
   const checkEndGame = (rows, cell, diagonals) => {
     const checkWin = () => {
+      console.log(board.log())
+     
       if (rows.includes("XXX") || rows.includes("OOO")) {
         return true;
       }
@@ -218,67 +231,120 @@ function game() {
     return false;
   };
 
-  // Main Game Loop
-  while (isPlaying === true) {
-    if (!currentPlayer) {
-      currentPlayer = player1;
-    }
-    const move = getMove();
-    makeMove(move.row, move.column, currentPlayer.sign);
-    const boardState = {
-      rows: board.getRows(),
-      columns: board.getColumns(),
-      diagonals: board.getDiagonals(),
-    };
-    if (
-      checkEndGame(boardState.rows, boardState.columns, boardState.diagonals)
-    ) {
-      endGame();
-      break;
-    }
-    board.render();
-    if (currentPlayer === player2) {
-      currentPlayer = player1;
-    } else {
-      currentPlayer = player2;
-    }
-  
-  }
-  
-}
+  return {
+    createPlayers,
+    getCurrentPlayer,
+    setCurrentPlayer,
+    makeMove,
+  };
+})();
 
-(function userInterface(){
-  const elements = ( ()=> {
+const userInterface = (() => {
+  const elements = (() => {
     return {
-      newGame: document.querySelector('#newGame'),
+      newGame: document.querySelector("#newGame"),
+      startGameButton: document.querySelector("#startGame"),
       player1: {
-        nameInput: document.querySelector('#playerName1'),
-        signSelect: document.querySelector('#playerSign1'),
-        scoreSpan: document.querySelector('#playerScore1')
+        nameInput: document.querySelector("#playerName1"),
+        signSelect: document.querySelector("#playerSign1"),
+        scoreSpan: document.querySelector("#playerScore1"),
       },
       player2: {
-        nameInput: document.querySelector('#playerName2'),
-        signSelect: document.querySelector('#playerSign2'),
-        scoreSpan: document.querySelector('#playerScore2')
+        nameInput: document.querySelector("#playerName2"),
+        signSelect: document.querySelector("#playerSign2"),
+        scoreSpan: document.querySelector("#playerScore2"),
       },
-      cells: document.querySelectorAll('.cell'),
-      cellsX: document.querySelectorAll('[data-sign="X"]'),
-      cellsO: document.querySelectorAll('[data-sign="O"]'),
-      resultDialog: document.querySelector('dialog#gameResult')
-    }
-  })()
-
+      cells: document.querySelectorAll(".cell"),
+      resultDialog: document.querySelector("dialog#gameResult"),
+    };
+  })();
+  console.log(elements.player1)
 
   // Event Handlers
   const handleCellClick = (e) => {
-    let element = e.target
-    const row = element.dataset.row
-    const column = element.dataset.cell
-    
-    
+    const element = e.target;
+    const row = element.dataset.row;
+    const column = element.dataset.cell;
+    const sign = game.getCurrentPlayer().sign;
+
+    if (!row || !column || !sign) throw "Error: varlieble is not set";
+    element.dataset.sign = sign;
+    game.makeMove(row, column, sign);
+    game.setCurrentPlayer();
+  };
+  const handleNewGame = (e) => {
+    playerName1 = "";
+    playerSign1 = "";
+    playerName2 = "";
+    playerSign2 = "";
+
+    elements.player1.nameInput.disabled = false;
+    elements.player1.signSelect.disabled = false;
+    elements.startGameButton.disabled = false;
+    elements.newGame.disabled = true;
+    elements.player1.nameInput.addEventListener("change", () => {
+      playerName1 = elements.player1.nameInput.value;
+    });
+    elements.player1.signSelect.addEventListener("change", () => {
+      playerSign1 = elements.player1.signSelect.value;
+
+      if (playerSign1 === "X") {
+        playerSign2 = "O";
+      } else if (playerSign1 === "O") {
+        playerSign2 = "X";
+      }
+
+      elements.player2.nameInput.disabled = false;
+      elements.player2.signSelect.value = playerSign2;
+      elements.player2.nameInput.focus();
+    });
+    elements.player2.nameInput.addEventListener("change", (e) => {
+      elements.newGame.disabled = false;
+      playerName2 = elements.player2.nameInput.value;
+    });
+    elements.startGameButton.addEventListener("click", () => {
+
+      elements.player1.signSelect.disabled = true;
+      elements.player1.nameInput.disabled = true;
+      elements.player2.nameInput.disabled = true;
+
+      game.createPlayers(
+        playerName1,
+        playerSign1,
+        0,
+        playerName2,
+        playerSign2,
+        0
+      );
+    });
+
+    elements.cells.forEach((cell) =>{
+      cell.addEventListener("click", handleCellClick)
+      cell.classList.remove('disabled')
+
+    }
+    );
+  };
+
+  // Utility functions
+  const reset = () =>{
+    for (const cell of elements.cells) {
+      cell.dataset.sign = ""
+      cell.classList.add('disabled')
+      elements.player1.nameInput = ""
+      elements.player1.signSelect = ""
+      elements.player2.nameInput = ""
+
+      
+    }
   }
+
   // Event Listeners
-  elements.cells.forEach(cell => cell.addEventListener("click", handleCellClick))
-})()
+  elements.newGame.addEventListener("click", handleNewGame);
+
+  return {
+    reset,
+  }
+})();
 
 // game();
